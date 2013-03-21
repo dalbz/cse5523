fd20af446941cdbf5578ef08983e8655b13d45c0
Dave <david.albert09@gmail.com>
2013-03-21 18:32:22 -0400

    Clustering and Dimensionality Reduction

---
+++ kmeans.py
@@ -0,0 +1,86 @@
+import sys
+import random
+import scipy
+from scipy.spatial.distance import sqeuclidean
+from scipy.sparse import *
+from scipy.sparse.linalg import *
+import numpy
+import matplotlib.pyplot as plt
+
+text_data = [line.strip() for line in open('reduced-data.txt')]
+data = [[float(value) for value in line.rstrip(',').split(',')] for line in text_data]
+
+labels = [line.strip() for line in open('artistList.txt')]
+
+# pre-process data
+
+r_data = numpy.array(data) 
+
+# get number of clusters
+
+num_clusters = int(sys.argv[1])
+
+# init random centers from points in dataset
+
+centers = numpy.array([r_data[random.randint(0, r_data.shape[0])] for i in range(0, num_clusters)])
+
+print centers.shape
+
+# init list of cluster labales
+
+clusters = [0 for i in range(0, r_data.shape[0]) ]
+
+print len(clusters)
+
+def update_clusters(clusters, centers, r_data, num_clusters):
+
+    for i in range(0, r_data.shape[0]) :
+
+        distances = numpy.array([sqeuclidean(r_data[i], centers[j]) for j in range(0, num_clusters)])
+
+        clusters[i] = numpy.argmin(distances)
+
+def update_centers(clusters, centers, r_data, num_clusters):
+
+    new_centers = numpy.array([[0 for j in range(0, r_data.shape[1])] for i in range(0, num_clusters)])
+    cluster_counts = [0 for i in range(0, num_clusters)]
+
+    for i in range(0, r_data.shape[0]):
+
+        current_cluster = clusters[i]
+
+        new_centers[current_cluster] += r_data[i]
+
+        cluster_counts[current_cluster] += 1
+
+    for n in range(0, num_clusters):
+
+        new_centers[n] /= float(cluster_counts[n])
+
+    return new_centers 
+
+n = 0
+delta = 99.0
+
+while (n < 50 and delta > 0.000001):
+
+    update_clusters(clusters, centers, r_data, num_clusters)
+    new_centers = update_centers(clusters, centers, r_data, num_clusters)
+
+    delta = 0.0
+
+    for i in range(0, num_clusters):
+        delta += sqeuclidean(centers[i], new_centers[i])
+
+    centers = new_centers
+
+    n += 1
+
+results = [[] for i in range(0, num_clusters)]
+
+for i in range(0, r_data.shape[0]):
+    results[clusters[i]].append(labels[i])
+
+for artist_list in results:
+    print artist_list
+    print '\n\n'
--- pca.py
+++ pca.py
@@ -1,15 +1,91 @@
-from matplotlib.mlab import PCA
-import numpy as np
+import scipy
+import sys
+from scipy.sparse import *
+from scipy.sparse.linalg import *
+import numpy
+import matplotlib.pyplot as plt
 
 tfidf = [line.strip() for line in open('tf-idfdata.txt')]
 data = [[float(value) for value in line.rstrip(',').split(',')] for line in tfidf]
 
-a = np.array(data) 
-U, s, V = np.linalg.svd(a, full_matrices=True)
+classes = [int(line.strip()) for line in open('classification.txt')]
+labels = [line.strip() for line in open('artistList.txt')]
 
-"""
 myData = numpy.array(data) 
-results = PCA(myData) 
 
-print results.Y 
-"""
+print myData.shape
+
+sparse = numpy.transpose(csc_matrix(myData))
+
+print 'data loaded'
+
+decomp = svds(sparse, 2)
+# result = (decomp[0]*decomp[1])
+result = decomp[0]
+
+print decomp[0].shape
+print decomp[1].shape
+print result.shape
+
+proj = numpy.transpose(sparse)*result
+
+print proj.shape
+
+reduced = proj.tolist()
+
+x_yes = []
+y_yes = []
+x_no = []
+y_no = []
+
+for i in range(0, 100):
+    if (classes[i] == 1):
+        x_yes.append(reduced[i][0]*-1)
+        y_yes.append(reduced[i][1]*-1)
+    else :
+        x_no.append(reduced[i][0]*-1)
+        y_no.append(reduced[i][1]*-1)     
+
+# Create 2-D Visualization of reduced data
+
+fig = plt.figure()
+sub = fig.add_subplot(1,1,1)
+sub.plot(x_yes, y_yes, 'b.', x_no, y_no, 'r.')
+
+for i in range(0, 100):
+    sub.annotate(
+        unicode(labels[i], "UTF-8"), 
+        xy = ((reduced[i][0]*-1), (reduced[i][1]*-1)), xytext = (-20, -20),
+        textcoords = 'offset points', ha = 'right', va = 'bottom',
+        bbox = dict(boxstyle = 'round,pad=0.5', fc = 'yellow', alpha = 0.5),
+        arrowprops = dict(arrowstyle = '->', connectionstyle = 'arc3,rad=0'))
+
+fig.set_size_inches(120,100)
+plt.savefig('2D-Vis.png',dpi=50)
+
+plt.show()
+
+# Export N-D dimensional data for use in K-Means
+
+output_file = open('reduced-data.txt', 'w')
+
+decomp = svds(sparse, int(sys.argv[1]))
+# result = (decomp[0]*decomp[1])
+result = decomp[0]
+
+print decomp[0].shape
+print decomp[1].shape
+print result.shape
+
+proj = numpy.transpose(sparse)*result
+
+print proj.shape
+
+reduced = proj.tolist()
+
+for row in reduced:
+    for value in row:
+        output_file.write(str(value) + ',')
+    output_file.write('\n')
+
+output_file.close()
